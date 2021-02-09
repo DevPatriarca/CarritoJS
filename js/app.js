d331a01d88c08befc8d15464bb447a4b5fce2e5c
Juan Jiménez <nefioxdev@gmail.com>
2021-02-09 11:59:11 -0600

    Agregando funcionalidades al carrito de compras

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -11,6 +12,43 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+
+function leerDatosCurso(curso) {
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+function carritoHTML() {
+
+    limpiarHTML();
+
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        contenedorCarrito.appendChild(row);     
+    });
+}
+
+function limpiarHTML() {
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
 }
